5458cf71bbb60cc4eacff9353bb1c3b475d46211
fanjunjie <923597516@qq.com>
2021-03-23 17:01:41 +0800

    测试-9

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ http.createServer(async (req, res) => {
         const projectDir = path.resolve(`./${data.repository.name}`)
         deleteFolderRecursive(projectDir)
 
-        console.log('克隆代码地址为:', `https://github.com/fan12306/${data.repository.name}.git ${projectDir}`)
+        console.log('克隆代码地址为:', `git//github.com/fan12306/${data.repository.name}.git ${projectDir}`)
         // 拉取仓库最新代码
-        execSync(`git clone https://github.com/fan12306/${data.repository.name}.git ${projectDir}`, {
+        execSync(`git clone git://github.com/fan12306/${data.repository.name}.git ${projectDir}`, {
             stdio: 'inherit',
         })
 
